dab7663b1ff153aed142f7162f7b072c1f6fe08e
Birlik Bekzat <119001855+bakebrlk@users.noreply.github.com>
2024-03-21 07:10:21 +0600

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  swiftProject
+//
+//  Created by bakebrlk on 21.03.2024.
+//
+
+import Foundation
+
+let taskScore: Int = 100
+var myScore: Int = taskScore
+let result = taskScore + myScore
+
+print(taskScore, myScore, result)
