3d499441546e9fab1e0483cb63f6223dc71c66f6
Birlik Bekzat <119001855+bakebrlk@users.noreply.github.com>
2024-03-21 08:22:14 +0600

    Update and rename sum.swift to firstModuleTask.swift

--- firstModuleTask.swift
+++ firstModuleTask.swift
@@ -1,5 +1,5 @@
 //
-//  sum.swift
+//  firstModuleTask.swift
 //  swiftProject
 //
 //  Created by bakebrlk on 21.03.2024.
@@ -10,9 +10,11 @@ import Foundation
 print("Введите число n:")
 if let input = readLine(), let n = Int(input) {
 
-    let sum = n * (n + 1) / 2
-
-    print(sum)
+    if 1 <= n && n <= 65535 {
+        
+        let sum = n * (n + 1) / 2
+        print(sum)
+    }
 } else {
     print("Ошибка ввода. Пожалуйста, введите целое число.")
 }
