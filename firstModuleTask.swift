721c7b8f3ac58124551c3e64df1d7017950dba2e
Birlik Bekzat <119001855+bakebrlk@users.noreply.github.com>
2024-03-21 08:41:19 +0600

    Update firstModuleTask.swift
    Complete the Word

--- firstModuleTask.swift
+++ firstModuleTask.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// sum-1-n
 print("Введите число n:")
 if let input = readLine(), let n = Int(input) {
 
@@ -19,3 +20,35 @@ if let input = readLine(), let n = Int(input) {
     print("Ошибка ввода. Пожалуйста, введите целое число.")
 }
 
+// Complete the Word
+func canConstruct(_ letters: String, _ word: String) -> Bool {
+    var letterCounts = [Character: Int]()
+    
+    for letter in letters {
+        letterCounts[letter, default: 0] += 1
+    }
+    
+    for char in word {
+        if let count = letterCounts[char], count > 0 {
+            letterCounts[char] = count - 1
+        }
+    }
+    
+    for letter in letters {
+        if letterCounts[letter]!  > 0 {
+            return false
+        }
+    }
+    
+    return true
+}
+
+print("Введите набор букв:")
+if let letters = readLine(), !letters.isEmpty {
+    print("Введите слово:")
+    if let word = readLine(), !word.isEmpty {
+        
+        let result = canConstruct(letters, word)
+        print(result)
+    }
+}
